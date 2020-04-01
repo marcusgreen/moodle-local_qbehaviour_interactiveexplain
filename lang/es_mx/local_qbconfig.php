af32cf0b20965d81ca0963782fdec67b9928e830
Germán Valero <gvalero@unam.mx>
2020-04-01 08:35:50 -0600

    copy English strings to es_mx

---
+++ lang/es_mx/local_qbconfig.php
@@ -0,0 +1,37 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Plugin strings are defined here.
+ *
+ * @package     local_qbconfig
+ * @copyright   Marcus Green 2020
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'Question behaviour config';
+$string['useeditor'] = 'Use editor';
+$string['useeditor_text'] = 'Turn on default html editor instead of plain text';
+$string['starthidden'] = 'Start hidden';
+$string['starthidden_text'] = 'The field is hidden at the start';
+$string['header'] = 'Header';
+$string['header_text'] = 'Header to student to give feedback';
+$string['details'] = 'Details';
+$string['details_text'] = 'Details to student to give feedback';
+$string['problem_with_question_header'] = 'Is there a problem with this question? Send feedback to your instructor here.';
+$string['problem_with_question_details'] = 'If you have feedback for this question, please type it below. The text will be sent to your instructor. You will be able to continue editing this when you have submitted your answer.';
