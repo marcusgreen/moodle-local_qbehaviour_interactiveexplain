eb3339f63ac2cd2efc2a64b1d9cead71aa1f7192
marcusgreen <marcusgreen@mailbox.org>
2020-04-10 20:06:24 +0100

    added null privacy provider

--- lang/en/local_qbehaviour_interactiveexplain.php
+++ lang/en/local_qbehaviour_interactiveexplain.php
@@ -34,6 +34,4 @@ $string['details_text'] = 'Details to student to give feedback';
 $string['problem_with_question_header'] = 'Is there a problem with this question? Send feedback to your instructor here.';
 $string['problem_with_question_details'] = 'If you have feedback for this question, please type it below. The text will be sent to your instructor. You will be able to continue editing this when you have submitted your answer.';
 $string['giveyourexplanation'] = 'Give your reasons';
-
-
-
+$string['privacy:metadata'] = 'The Interactive with explanation question behaviour  settings plugin does not store any personal data.';
