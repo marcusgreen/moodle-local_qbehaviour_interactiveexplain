96e714babc75ed3236118f39887f1e24122642da
marcusgreen <marcusgreen@mailbox.org>
2020-06-13 08:25:44 +0100

    strings

--- lang/en/local_qbehaviour_interactiveexplain.php
+++ lang/en/local_qbehaviour_interactiveexplain.php
@@ -35,3 +35,4 @@ $string['problem_with_question_header'] = 'Is there a problem with this question
 $string['problem_with_question_details'] = 'If you have feedback for this question, please type it below. The text will be sent to your instructor. You will be able to continue editing this when you have submitted your answer.';
 $string['giveyourexplanation'] = 'Give your reasons';
 $string['privacy:metadata'] = 'The Interactive with explanation question behaviour  settings plugin does not store any personal data.';
+$string['explanation'] = 'Explanation';
