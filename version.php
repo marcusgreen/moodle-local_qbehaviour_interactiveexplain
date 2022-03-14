1233ca043cdea3ebddfa543865257b02f494e496
Marcus Green <marcusavgreen@gmail.com>
2022-03-14 20:24:25 +0000

    version bump

--- version.php
+++ version.php
@@ -27,6 +27,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_qbehaviour_interactiveexplain';
 $plugin->release = '1.0.0';
-$plugin->version = 2020061300;
+$plugin->version = 2022031400;
 $plugin->requires = 2018120300;
 $plugin->maturity = MATURITY_STABLE;
